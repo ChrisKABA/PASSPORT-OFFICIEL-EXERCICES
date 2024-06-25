f5ac3b7fd3acc693f1091bf319c16a9c0a7a4077
chrisKABA <chriskaba7@gmail.com>
2024-06-25 12:30:38 +0100

    fh

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require('dotenv').config();//Cette ligne charge les variables d'environnement à partir d'un fichier .env (s'il existe) dans l'environnement de l'application.
+//Cela permet d'utiliser des variables sensibles (comme les identifiants de base de données, les clés API, etc.) sans les exposer directement dans le code.
 
-var createError = require('http-errors');
+var createError = require('http-errors');//Cette ligne importe le module http-errors, qui fournit des fonctions pour créer des erreurs HTTP standard.
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var cookieParser = require('cookie-parser');//Cette ligne importe le middleware cookie-parser, qui sera utilisé pour analyser et manipuler les cookies dans votre application.
+var logger = require('morgan');//Cette ligne importe le middleware morgan, qui sera utilisé pour journaliser les requêtes HTTP entrantes dans votre application.
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');//Cette ligne importe le routeur défini dans le fichier ./routes/index.js. Ce routeur contiendra probablement les routes principales de l'application, comme la page d'accueil, la page "À propos", etc.
+var authRouter = require('./routes/auth');//Ce routeur contiendra probablement les routes liées à l'authentification des utilisateurs, comme la connexion, l'inscription, la réinitialisation du mot de passe, etc.
 
 var app = express();
 
-app.locals.pluralize = require('pluralize');
+app.locals.pluralize = require('pluralize');//cette ligne de code ajoute une fonction de pluralisation pluralize aux variables locales de l'application Express (app.locals).
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
